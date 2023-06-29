17a135297a72a2230166e81a833fad5aafd0dc85
icweaver <weaveric@gmail.com>
2023-06-29 15:40:22 -0700

    up

---
+++ controlflow-rs/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let num = 1;
+    let val = 5;
+
+    let x = if num > val {
+        1
+    } else if num < val {
+        0.0
+    } else {
+        -1
+    };
+
+    //dbg!(x);
+
+    // let x = if num > 5 {10} else {"low"};
+
+    // println!("{x}");
+}
