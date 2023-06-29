d7e73490fe29a630fd10feccb9cfc5b5f9be6a93
icweaver <weaveric@gmail.com>
2023-06-29 16:15:00 -0700

    up

--- controlflow-rs/src/main.rs
+++ controlflow-rs/src/main.rs
@@ -2,15 +2,27 @@ fn main() {
     let num = 1;
     let val = 5;
 
-    let x = if num > val {
+    let mut x = if num > val {
         1
     } else if num < val {
-        0.0
+        0
     } else {
         -1
     };
 
-    //dbg!(x);
+    dbg!(x);
+
+    loop {
+        x += 1;
+
+        if x == 11 {break};
+    }
+
+    dbg!(x);
+
+    for i in (1..=10).rev() {
+        println!("{i}");
+    }
 
     // let x = if num > 5 {10} else {"low"};
 
