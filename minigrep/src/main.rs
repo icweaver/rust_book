b244b8b9d098bf5c1b47ee461d29e6a509ca375c
icweaver <weaveric@gmail.com>
2023-07-16 01:29:35 -0700

    up

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error parsing command line args: {err}");
+        eprintln!("Error parsing command line args: {err}");
         process::exit(1);
     });
 
@@ -14,7 +14,7 @@ fn main() {
     // about the successful unit return type of `run` here,
     // only the return type of the error case
     if let Err(e) = minigrep::run(config) {
-        println!("Error searching: {e}");
+        eprintln!("Error searching: {e}");
         process::exit(1);
     }
 
