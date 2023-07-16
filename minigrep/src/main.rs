35d13722062fb28b86973830f3fcd3c720bf1f9c
icweaver <weaveric@gmail.com>
2023-07-15 23:07:16 -0700

    up

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,18 +6,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        println!("Error parsing command line args: {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     // Using this instead of `.unrap_or_else` because we do not care
     // about the successful unit return type of `run` here,
     // only the return type of the error case
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        println!("Error searching: {e}");
         process::exit(1);
     }
 
