a8afae637ddcf6b8a9d43e4dbd2f96387a239eb6
icweaver <weaveric@gmail.com>
2023-07-15 22:42:45 -0700

    up

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
-use std::{env, fs, process};
-use std::error::Error;
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,34 +16,9 @@ fn main() {
     // Using this instead of `.unrap_or_else` because we do not care
     // about the successful unit return type of `run` here,
     // only the return type of the error case
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone(); // Avoids dealing with lifetimes
-        let file_path = args[2].clone(); // Can optimize later
-
-        Ok(Config { query, file_path })
-    }
 }
